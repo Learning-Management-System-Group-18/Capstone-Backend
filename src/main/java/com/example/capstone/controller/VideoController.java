94c7f19729d4da57fed02a153aa6dd514add7ffb
Agus Abdul Malik <magusabdul@gmail.com>
2022-06-20 18:51:52 +0700

    add fitur update video and delete video

--- src/main/java/com/example/capstone/controller/VideoController.java
+++ src/main/java/com/example/capstone/controller/VideoController.java
@@ -2,8 +2,10 @@ package com.example.capstone.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,5 +29,15 @@ public class VideoController {
     public ResponseEntity<Object> createNewVideo(@RequestParam(value = "sectionId")Long sectionId,
                                                     @RequestBody VideoDto request){
         return videoService.createNewVideo(sectionId,request);
+    }
+    @PutMapping("/admin/video")
+    public ResponseEntity<Object> updateVideo(@RequestParam(value = "videoId") Long videoId,
+                                                @RequestBody VideoDto request){
+        return videoService.updateVideo(videoId, request);
+    }
+
+    @DeleteMapping("/admin/video")
+    public ResponseEntity<Object> deleteVideo(@RequestParam(value = "id") Long id){
+        return videoService.deleteById(id);
     }
 }
--- src/main/java/com/example/capstone/service/VideoService.java
+++ src/main/java/com/example/capstone/service/VideoService.java
@@ -63,4 +63,45 @@ public class VideoService {
             return ResponseUtil.build(ResponseCode.UNKNOWN_ERROR, null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    public ResponseEntity<Object> updateVideo(Long videoId, VideoDto request){
+        log.info("Executing update video");
+        try {
+            Optional<Video> optionalVideo = videoRepository.findById(videoId);
+            if (optionalVideo.isEmpty()) {
+                log.info("Video with Id [{}] not found", videoId);
+                return ResponseUtil.build(ResponseCode.DATA_NOT_FOUND, null, HttpStatus.BAD_REQUEST);
+            }
+            Optional<Section> section = sectionRepository.findById(optionalVideo.get().getSection().getId());
+            optionalVideo.ifPresent(video -> {
+                video.setId(videoId);
+                video.setTitle(request.getTitle());
+                video.setDescription(request.getDescription());
+                video.setLink(request.getLink());
+                video.setSection(section.get());;
+                videoRepository.save(video);
+            });
+            log.info("Successfully update video");
+            return ResponseUtil.build(ResponseCode.SUCCESS, mapper.map(optionalVideo.get(), VideoDto.class), HttpStatus.OK);
+        } catch (Exception e){
+            log.error("An Error occurred while trying to updated video. Error:{}",e.getMessage());
+            return ResponseUtil.build(ResponseCode.UNKNOWN_ERROR, null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Object> deleteById(Long id){
+        log.info("Executing delete existing video");
+        try {
+            Optional<Video> optionalVideo = videoRepository.findById(id);
+            if (optionalVideo.isEmpty()){
+                log.info("Video with Id : [{}] is not found",id);
+                return ResponseUtil.build(ResponseCode.DATA_NOT_FOUND,null,HttpStatus.BAD_REQUEST);
+            }
+            videoRepository.delete(optionalVideo.get());
+            log.info("Successfully delete video with ID : [{}]", id);
+            return ResponseUtil.build(ResponseCode.SUCCESS,null,HttpStatus.OK);
+        } catch (Exception e){
+            log.info("An error occurred while trying to delete existing video. Error : {}",e.getMessage());
+            return ResponseUtil.build(ResponseCode.UNKNOWN_ERROR, null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
